d8f71d8289ea010efb7d32ba373548c2ec6e7123
Justin Beynon <JMB335@gmail.com>
2020-05-18 20:09:04 -0500

    Added comments to HTML and JS files

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 
     
-
-function playRound(playerSelection){ //Function to determine computer player's choice
-    function computerPlay(){
+//Function that pays a round of RPS. Player's choice is passed through
+function playRound(playerSelection){ 
+    function computerPlay(){                                  //Function that determines the computer's choice for the round
     let rando = Math.floor(Math.random() * Math.floor(3));
         if (rando == 1) {return 'Rock'}
         else if (rando == 2) {return 'Paper'}
         else {return 'Scissors'}
         }   
-    let computerSelection = computerPlay() //Evaluates value from click against computer's choice
+     
+    let computerSelection = computerPlay()  //Stores computer's choice is a variable
         if (computerSelection.toLowerCase() == playerSelection.toLowerCase()){return `Tied Game! Computer played ${computerSelection.toUpperCase()} and you played ${playerSelection.toUpperCase()}`}
         else if (
             (computerSelection == 'Rock' && playerSelection.toLowerCase() == 'paper') ||
             (computerSelection == 'Paper' && playerSelection.toLowerCase() == 'scissors') || 
             (computerSelection == 'Scissors' && playerSelection.toLowerCase() == 'rock')
-            ){return `You won! Computer played ${computerSelection.toUpperCase()} and you played ${playerSelection.toUpperCase()}`}
-        else return `You lost! Computer played ${computerSelection.toUpperCase()} and you played ${playerSelection.toUpperCase()}`;
+            ){return `You won this round! You played ${playerSelection.toUpperCase()} and computer played ${computerSelection.toUpperCase()}`} //Updates html text to display the result of the round
+        else return `You lost this round! You played ${playerSelection.toUpperCase()} and computer played ${computerSelection.toUpperCase()}`;
 }
 
 let resultRecorder = document.querySelector("#result-recorder")
@@ -25,23 +26,19 @@ let computerScoreText = document.querySelector("#comp-score")
 let playerScore = 0
 let computerScore = 0
 
-if (playerScore === 5) {finalResultText.textContent = "Congrats you won!"}
-    else if (computerScore === 5) {finalResultText.textContent = "Too bad, computer won!"}
-
-
-
+//Adds listener to each button. On click, the ID of the button is passed through playRound function as the player's choice
 document.querySelectorAll(".player-button").forEach(item => {
   item.addEventListener('click', event => {
     let round = playRound(event.target.id);
 
-    if (playerScore < 5 && computerScore < 5){
+    if (playerScore < 5 && computerScore < 5){                  //Checks if the score is already 5
         resultRecorder.textContent = round
         if (round.includes("won")){playerScore++}
             else if (round.includes("lost")){computerScore++}
         playerScoreText.textContent = playerScore.toString()
         computerScoreText.textContent = computerScore.toString()
          }
-    if (playerScore === 5) {finalResultText.textContent = "Congrats you won!"}
+    if (playerScore === 5) {finalResultText.textContent = "Congrats you won!"}                  //Checks to see if 5 rounds have been played. If so, prints final results
         else if (computerScore === 5) {finalResultText.textContent = "Too bad, computer won!"}
 
 })
